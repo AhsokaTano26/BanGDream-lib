59ca7dffa244acfff11a6eb8c9297f56a3ffd0b0
Tano <github@tano.asia>
2026-06-21 02:10:01 +0800

    fix: 修复部分问题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
           options: { cacheName: 'gstatic-fonts-cache', expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 } },
         },
       ],
-maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+      maximumFileSizeToCacheInBytes: 100 * 1024 * 1024,
     },
     client: { installPrompt: true },
   },
